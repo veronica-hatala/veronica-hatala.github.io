0f1a8aaa06d78a59f9c70b77186eb55108bf505a
Veronica Hatala <veronica.hatala@gmail.com>
2025-01-04 10:54:03 -0500

    Editing SCSS file structure and deleting unrequired files

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -4,7 +4,7 @@ export default function AboutMe() {
         <h2 className="about-heading">About Me</h2>
         <p className="about-content">
         Hi! My name is Veronica and I'm a software developer from New Jersey, USA. 
-        I'm a front end developer with professional experience with PHP, WordPress, 
+        I have professional experience with PHP, WordPress, 
         JavaScript, React, HTML and CSS/Sass. I studied Computer Science at Saint Mary's 
         University in Halifax, Nova Scotia.
         </p>
